866e1f44f176ace002a2be1fb60da84c8f92dba9
Tim Bijnen <tim.bijnen@login.greenhousegroup.com>
2019-03-01 09:51:14 +0100

    Add object support

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const uniqid = require("uniqid");
-const ADDONS = [ "SUCCESS", "ERROR" ];
+const EXTENSIONS = [ "SUCCESS", "ERROR" ];
 
-module.exports = ( actions, addons = ADDONS ) =>
+const create = ( {
+    actions = [],
+    asyncActions = [],
+    extensions = EXTENSIONS,
+} ) => {
+    return {
+	...createActions( actions ),
+	...createAsyncActions( asyncActions, extensions ),
+    }
+};
+
+const createActions = actions =>
+    actions.reduce( ( a, item ) => {
+        if ( typeof item !== "string" ) throw new Error( "action name must be a string" );
+	const id = uniqid();
+        return { ...a, [ item ]: `${ id }_${ item }` };
+    }, {} );
+
+const createAsyncActions = ( actions, extensions ) =>
     actions.reduce( ( a, item ) => {
         if ( typeof item !== "string" ) throw new Error( "action name must be a string" );
 	const id = uniqid();
         const action = { ...a, [ item ]: `${ id }_${ item }` };
-        return ( addons || [] ).reduce( ( b, addon ) => ( {
+        return extensions.reduce( ( b, addon ) => ( {
             ...b,
             [ `${ item }_${ addon }` ]: `${ id }_${ item }_${ addon }`,
         } ), action );
     }, {} );
 
+module.exports = create;
+module.exports.createActions = createActions;
+module.exports.createAsyncActions = createAsyncActions;
--- index.test.js
+++ index.test.js
@@ -1,15 +1,26 @@
 const actionType = require("./index");
 
+const actions = {
+    actions: [ "UPDATE" ],
+    asyncActions: [ "LOAD", "DELETE" ],
+    extensions: [ "ERROR", "SUCCESS" ],
+};
+
+
+
 describe( "Test", () => {
     it( "can generate unique action types", ( ) => {
-        const types = actionType( [ "LOAD" ] );
+        const types = actionType( { asyncActions: [ "LOAD" ] } );
         expect( types ).toHaveProperty( "LOAD" );
         expect( types ).toHaveProperty( "LOAD_SUCCESS" );
 	expect( types ).toHaveProperty( "LOAD_ERROR" );
     } );
 
-	it( "can generate unique action types", ( ) => {
-        const types = actionType( [ "LOAD" ], [ "FAIL", "SUCCEEDED" ] );
+    it( "can generate unique action types", ( ) => {
+	const types = actionType( {
+	    asyncActions: [ "LOAD" ],
+	    extensions: [ "FAIL", "SUCCEEDED" ],
+	} );
         expect( types ).toHaveProperty( "LOAD" );
         expect( types ).toHaveProperty( "LOAD_SUCCEEDED" );
 	expect( types ).toHaveProperty( "LOAD_FAIL" );
@@ -17,25 +28,26 @@ describe( "Test", () => {
 
 
     it( "can generate unique action types", ( ) => {
-        const types = actionType( [ "LOAD" ], [] );
+        const types = actionType( { actions: [ "LOAD" ] } );
         expect( types ).toHaveProperty( "LOAD" );
         expect( types ).not.toHaveProperty( "LOAD_SUCCESS" );
     } );
 
     it( "can generate unique action types", ( ) => {
-        const types = actionType( [ "LOAD" ], null );
+        const types = actionType( { actions: [ "LOAD" ] } );
         expect( types ).toHaveProperty( "LOAD" );
         expect( types ).not.toHaveProperty( "LOAD_SUCCESS" );
     } );
-
-    it( "can generate unique action types", ( ) => {
-        const types = actionType( [ "LOAD" ], [ "TEST" ] );
-        expect( types ).toHaveProperty( "LOAD" );
-        expect( types ).toHaveProperty( "LOAD_TEST" );
-    } );
-
+    
     it( "gives a neat error", ( ) => {
-        expect( () => actionType( [ "LOAD", [] ] ) ).toThrow( "action name must be a string" );
+        expect( () => actionType( { actions: [ "LOAD", [] ] } ) ).toThrow( "action name must be a string" );
+    } );
+
+    it( "Can generate from an object", () => {
+	const types = actionType( actions );
+        expect( types ).toHaveProperty( "UPDATE" );
+        expect( types ).toHaveProperty( "LOAD" );   
+	expect( types ).toHaveProperty( "LOAD_SUCCESS" );
     } );
 } );
 
